33bd32ea1810ccc74b99a02c53400d993c199599
Scott T Weaver <scott.t.weaver@gmail.com>
2013-01-16 12:49:07 -0600

    Reorginzation of files.  Move environment check from ruby-sapjco.rb to the new ruby-sapjco-env.rb. Remove the ruby-sapjco-function.rb file, moving all its contents to ruby-sapjco.rb.

--- lib/ruby-sapjco.rb
+++ lib/ruby-sapjco.rb
@@ -1,19 +1,146 @@
-require 'java'
+# require 'ruby-sapjco'
+require 'ruby-sapjco-config'
+require 'ruby-sapjco-assist'
 require 'logging-facade'
+require 'haml'
+require 'launchy'
 
-#Begin by testing to see if SAPJCo is available in the class path.
-begin
-  require 'sapjco3.jar'
-  java_import com.sap.conn.jco.ext.Environment
-  java_import Java::ComSapConnJcoRt::DefaultListMetaData
-  java_import Java::ComSapConnJcoRt::DefaultRecordMetaData
-  LoggingFacade::Logger.logger.info "sapjco3.jar successfuly loaded!"
-rescue Exception => e
-  puts e
-  raise( %q{
-           The sapjco3.jar could not be located on the classpath.  Either:
-           - Add it to you CLASSPATH environment variable.
-           - Make sure it is part of the classpath of the JVM running JRuby.
-           - Create a ruby that contains the 'sapjco3.jar' in the /lib directory
-  and install the gem locally.})
+module SapJCo 
+  class Function
+    include  LoggingFacade::Logger
+
+    def initialize(function_name, destination_name=SapJCo::Configuration.configuration[:default_destination])
+      @function_name = function_name
+      @destination_assistant = DestinationAssistant.new(destination_name)
+      @func = @destination_assistant.destination.repository.get_function(@function_name.to_s)
+    end
+
+
+    # Executes the SAP RFC.  Optionally: you can pass in a block that takes
+    # a hash which can be used to set the import parameter list for this function
+    # call.
+    def execute
+      import_params = {}
+      table = {}
+      yield(import_params, table) if block_given?
+
+      raise "RFC #{@function_name.to_s} is not available on the target system." if @func.nil?
+
+      imp_list = @func.get_import_parameter_list
+
+      import_params.each do |key, value|
+        imp_list.set_value(key.to_s, value)
+      end
+
+      @func.execute @destination_assistant.destination
+
+      out = parse_sap_record_structure @func.get_export_parameter_list
+      out.merge!(parse_sap_record_structure(@func.get_table_parameter_list))
+      out
+    end
+
+    def metadata
+      out = {}
+      template = FunctionMetaData.new(@func)
+      out[:function]=template.name
+      out[:import_parameters]=template.import_params_info
+      out[:export_parameters]=template.export_params_info
+      out[:tables]=template.table_info
+      out
+    end
+
+    def help(open=false)
+      template = File.read("#{File.dirname(__FILE__)}/../templates/function_doc.haml")
+      engine = Haml::Engine.new(template)
+      html = engine.render DocumentationHelper.new, :metadata => metadata
+
+      if open
+        File.open("#{@function_name}.html", "w") do |file|
+          file.write html
+          logger.info "Help file path #{file.path}"
+          Launchy.open(file.path)
+        end
+      else
+        html
+      end
+
+    end
+
+
+    # Recursively converts JCoRecord types to Ruby Hashes and Arrays (JCoTable instances).
+    def parse_sap_record_structure(field_list)
+      out = Hash.new
+      field_list.each do |field|
+        if field.value.class.include?(com.sap.conn.jco.JCoTable)
+          table = field.get_table
+          table_array = []
+          begin
+            table_array << parse_sap_record_structure(table)
+          end while table.next_row
+          out[field.name.to_sym] = table_array
+        elsif  field.value.class.include?(com.sap.conn.jco.JCoRecord)
+          out[field.name.to_sym] = parse_sap_record_structure field.value
+        else
+          out[field.name.to_sym] = field.value
+        end
+      end unless field_list.nil?
+      out
+    end
+  end
+
+  class FunctionMetaData
+
+    def initialize(function)
+      @template = function.get_function_template
+      @import_params = @template.get_import_parameter_list
+      @export_params = @template.get_export_parameter_list
+      @tables = @template.get_table_parameter_list
+    end
+
+    def name
+      @template.get_name
+    end
+
+    def table_info
+      parse_parameters @tables
+    end
+
+    def import_params_info
+
+      parse_parameters @import_params
+    end
+
+    def export_params_info
+      parse_parameters @export_params
+    end
+
+    def parse_parameters(parameter_list)
+      out = {}
+      parameter_list.each do |param|
+        info = {}
+        info[:type] = param.type
+        info[:description] = param.description
+        #info[:import?] = param.import?
+        if param.fields?
+          fields = {}
+          info[:fields] = fields
+          param.each do |column|
+            fields[column.name.to_sym]={:type=>column.type,
+                                        :description=>column.description}
+          end
+        end
+        out[param.name.to_sym]=info
+      end unless parameter_list.nil?
+      out
+    end
+
+  end
+
+  class DocumentationHelper
+    def render(template_name, args)
+      template = File.read("#{File.dirname(__FILE__)}/../templates/#{template_name.to_s}.haml")
+      engine = Haml::Engine.new(template)
+      engine.render Object.new, args
+    end
+  end
 end
--- spec/ruby-sapjco/ruby-sapjco_spec.rb
+++ spec/ruby-sapjco/ruby-sapjco_spec.rb
@@ -1,7 +1,5 @@
 require_relative "../spec_helper.rb"
-
-require 'ruby-sapjco-config'
-require 'ruby-sapjco-function'
+require 'ruby-sapjco'
 require "yaml"
 
 
@@ -22,3 +20,66 @@ describe SapJCo::RubyDestinationDataProvider do
   end
 
 end
+
+
+describe SapJCo::Function do
+
+  it "should convert ugly SAPJCO Java structs into beautimous Ruby ones" do
+    func =  SapJCo::Function.new(:STFC_CONNECTION)
+
+    out = func.execute do |params|
+      params[:REQUTEXT] = 'Hello SAP!'
+    end
+
+    out[:ECHOTEXT].should eq 'Hello SAP!'
+  end
+
+  it "should handle SAP structures correctly" do
+    func =  SapJCo::Function.new(:RFC_SYSTEM_INFO)
+
+    out = func.execute
+    out[:RFCSI_EXPORT].should_not be nil
+    out[:RFCSI_EXPORT][:RFCHOST2].should eq 'saperqapp1'
+  end
+
+  it "should handle tables correctly" do
+    func =  SapJCo::Function.new(:BAPI_COMPANYCODE_GETLIST)
+
+    out = func.execute
+    out[:COMPANYCODE_LIST].class.should eq Array
+    out[:COMPANYCODE_LIST][0][:COMP_CODE].should eq EXPECTATIONS[:company_code_0]
+  end
+
+  it "should have metadata available" do
+    company_code_rfc =  SapJCo::Function.new(:BAPI_COMPANYCODE_GETLIST)
+    sys_info_rfc =  SapJCo::Function.new(:RFC_SYSTEM_INFO)
+
+    company_code_rfc.metadata[:function].should eq'BAPI_COMPANYCODE_GETLIST'
+    company_code_rfc.metadata[:import_parameters].length.should eq 0
+    company_code_rfc.metadata[:tables][:COMPANYCODE_LIST][:fields][:COMP_CODE][:type].should == 'CHAR'
+    company_code_rfc.metadata[:tables][:COMPANYCODE_LIST][:fields][:COMP_CODE][:description].should == 'Company Code'
+    company_code_rfc.metadata[:export_parameters][:RETURN][:type].should == 'STRUCTURE'
+    company_code_rfc.metadata[:export_parameters][:RETURN][:fields][:CODE][:type].should == 'CHAR'
+    #company_code_rfc[:tables]
+  end
+
+  it "should create html documentation" do
+    company_code_rfc =  SapJCo::Function.new(:BAPI_COMPANYCODE_GETLIST)
+    company_code_rfc.help true
+    install_config = SapJCo::Function.new(:Z_INSTALL_CONFIG)
+    install_config.help true
+  end
+
+  it "should support failing over to an alternate server" do
+    rfc =  SapJCo::Function.new(:Z_INSTALL_CONFIG)
+
+    result = rfc.execute do |inp|
+      inp[:MQUOTFLG] = "X"
+      inp[:FINSTNO]='0001090130'
+    end
+
+    result.should_not be nil
+    result[:TMATTAB].length.should > 0
+  end
+end
+
